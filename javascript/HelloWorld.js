b30e9e503657ad1f03958dc979da0f7d9a25c71b
sumedha-2323 <sumedhajammula@gmail.com>
2023-01-12 13:29:41 +0530

    commit

---
+++ javascript/HelloWorld.js
@@ -0,0 +1,6 @@
+function externalJS()
+{
+     console.log("Linking an external javascript file(helps to reduce redundancy)");
+     alert("Linking an external javascript file(helps to reduce redundancy)");
+}
+externalJS()
