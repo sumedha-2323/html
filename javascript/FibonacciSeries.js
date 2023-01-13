b7e69f9c1cf90fa51c60d27eb5c46f7ae75bdcdd
sumedha-2323 <sumedhajammula@gmail.com>
2023-01-13 19:19:30 +0530

    js

---
+++ javascript/FibonacciSeries.js
@@ -0,0 +1,15 @@
+var v1;
+function fibo()
+{
+    var v1= 10;
+    var v2= "Hello"
+    var v3= true;
+    var v4= [10, 20, 30];
+
+    document.writeln("v1: "+typeof(v1));
+    document.writeln("<br>v2: "+typeof(v2));
+    document.writeln("<br>v3: "+typeof(v3));
+    document.writeln("<br>v4: "+typeof(v4));
+}
+fibo();
+console.log(v1);
